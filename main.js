b52aa683db788fab69ac46244cbe49c26ca383d9
Arsalan Khattak <me@khattak.dev>
2024-06-06 16:07:39 +0500

    final with json data

--- main.js
+++ main.js
@@ -1,34 +1,83 @@
-import { Gantt } from "@bryntum/gantt";
+import {
+  Gantt,
+  AssignmentField,
+  WalkHelper,
+  StringHelper,
+  ProjectModel,
+} from "@bryntum/gantt";
 import "./style.css";
+
+const project = new ProjectModel({
+  autoLoad: true,
+  autoSync: true,
+  transport: {
+    load: {
+      url: "data.json",
+    },
+  },
+
+  listeners: {
+    beforeLoadApply: ({ response }) => {
+      WalkHelper.preWalk(
+        response.tasks.rows[0],
+        (n) => n.children,
+        (task) => {
+          task.priority = ["High", "Low", "Medium"][
+            Math.floor(Math.random() * 3)
+          ];
+        }
+      );
+    },
+  },
+});
 
 const gantt = new Gantt({
   appendTo: "app",
-  startDate: new Date(2022, 0, 1),
-  endDate: new Date(2022, 0, 10),
-  columns: [{ type: "name", width: 160 }],
-  project: {
-    tasksData: [
-      {
-        id: 1,
-        name: "Write docs",
-        expanded: true,
-        children: [
-          {
-            id: 2,
-            name: "Proof-read docs",
-            startDate: "2022-01-02",
-            endDate: "2022-01-09",
+  resourceImageFolderPath: "./users/",
+  columns: [
+    { type: "name", width: 250 },
+    {
+      type: "resourceassignment",
+      width: 250,
+      showAvatars: true,
+      editor: {
+        type: AssignmentField.type,
+        picker: {
+          height: 350,
+          width: 450,
+          features: {
+            filterBar: true,
+            group: "resource.city",
+            headerMenu: false,
+            cellMenu: false,
           },
-          {
-            id: 3,
-            name: "Release docs",
-            startDate: "2022-01-09",
-            endDate: "2022-01-10",
-          },
-        ],
+          // The extra columns are concatenated onto the base column set.
+          columns: [
+            {
+              text: "Calendar",
+              // Read a nested property (name) from the resource calendar
+              field: "resource.calendar.name",
+              filterable: false,
+              editor: false,
+              width: 85,
+            },
+          ],
+        },
       },
-    ],
+    },
+    {
+      field: "priority",
+      text: "Priority",
+      type: "template",
+      width: 120,
+      template: ({ value = "" }) => StringHelper.xss`
+          <div class="b-prio b-prio-${value.toLowerCase()}">${value}</div>`,
+      editor: {
+        type: "dropdown",
+        items: ["Low", "Medium", "High"],
+      },
+    },
+  ],
 
-    dependenciesData: [{ fromTask: 2, toTask: 3 }],
-  },
+  project: project,
 });
